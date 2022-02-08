62ceba78176fcdacabfb7ac3092880830cf0cdfe
Amish Menon <amishmenon@gmail.com>
2022-02-07 21:31:09 -0600

    adding build folder

---
+++ precache-manifest.1b10c4ad9591d4f811a48e7c34539e87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0682563374c57f7b251d9ad5ba982327",
+    "url": "/static/media/logo.06825633.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a82c86438b25c0e8a801",
+    "url": "/static/js/main.a82c8643.chunk.js"
+  },
+  {
+    "revision": "5f2cd54615d0bbd78048",
+    "url": "/static/js/1.5f2cd546.chunk.js"
+  },
+  {
+    "revision": "a82c86438b25c0e8a801",
+    "url": "/static/css/main.7f5f930b.chunk.css"
+  },
+  {
+    "revision": "5f2cd54615d0bbd78048",
+    "url": "/static/css/1.e73fd4a3.chunk.css"
+  },
+  {
+    "revision": "b8b62a923f1c02115e03defa0c298804",
+    "url": "/index.html"
+  }
+];
